0ff8a4ae0951a524fc0f72e8efba00a66c83bbbe
Masataka Kohagura <masalinkinpark@gmail.com>
2018-07-14 13:27:43 +0900

    長押し対応ボタンの実装

---
+++ Assets/Clione/UI/ButtonEventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clione.UI
+{
+    [Flags]
+    public enum ButtonEventType
+    {
+        None = 0,
+        ClickDown = 1,
+        StartLongTap = 1 << 1,
+        LongTap = 1 << 2,
+        EndLongTap = 1 << 3,
+        Decide = 1 << 4,
+        ClickUp = 1 << 5,
+    }
+}
